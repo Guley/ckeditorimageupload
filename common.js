4f5f2703605c63db082efcd294495c0d29a284b6
Gulshan <gul2787@gmail.com>
2019-05-06 10:41:02 +0530

    Add files via upload

--- common.js
+++ common.js
@@ -1,25 +1,49 @@
-jQuery(document).ready(function () {
-  
-   CKEDITOR.replace( 'story',
-                {
-                    filebrowserBrowseUrl :'imageslist.php',
-                    filebrowserImageBrowseUrl : 'imageslist.php',
-                    filebrowserFlashBrowseUrl :'imageslist.php',
-                    filebrowserUploadUrl  :'upload.php',
-                    filebrowserImageUploadUrl : 'upload.php',
-                    filebrowserFlashUploadUrl : 'upload.php'
- 
-        });
-
-});
-
-function copytext(image){
-   var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($('#'+image).data('link')).select();
-    document.execCommand("copy");
-    $temp.remove();
-    window.top.close();
-  /* Alert the copied text */
-  alert("Link Copied: ");
+  function confirmDelete(url){
+	swal({
+	  title: "Are you sure?",
+	  text: "You will not be able to recover!",
+	  type: "warning",
+	  showCancelButton: true,
+	  confirmButtonClass: "btn-danger",
+	  confirmButtonText: "Yes, remove it!",
+	  cancelButtonText: "No, cancel!",
+	  closeOnConfirm: false,
+	  closeOnCancel: true
+	},
+	function(isConfirm) {
+	  if (isConfirm) {
+	  	window.location.href = url;
+	  } 
+	});
 }
+
+
+CKEDITOR.replace( "blog_desc", {
+        filebrowserBrowseUrl: BASE_URL+'http://localhost/laurajanestylist/portal/ckfinder/ckfinder.html',
+    filebrowserImageBrowseUrl: BASE_URL+'/ckfinder/ckfinder.html?Type=Images',
+    filebrowserUploadUrl: BASE_URL+'/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
+    filebrowserImageUploadUrl: BASE_URL+'/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+    filebrowserWindowWidth: '1000',
+    filebrowserWindowHeight: '700'
+ });
+
+CKEDITOR.replace( "short_desc", {
+    filebrowserBrowseUrl: BASE_URL+'http://localhost/laurajanestylist/portal/ckfinder/ckfinder.html',
+    filebrowserImageBrowseUrl: BASE_URL+'/ckfinder/ckfinder.html?Type=Images',
+    filebrowserUploadUrl: BASE_URL+'/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
+    filebrowserImageUploadUrl: BASE_URL+'/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+    filebrowserWindowWidth: '1000',
+    filebrowserWindowHeight: '700'
+	     // filebrowserBrowseUrl      : BASE_URL+'imageslist',
+      //   filebrowserImageBrowseUrl : BASE_URL+'imageslist',
+      //   filebrowserFlashBrowseUrl : BASE_URL+'imageslist',
+      //   filebrowserUploadUrl      : BASE_URL+'imageupload',
+      //   filebrowserImageUploadUrl : BASE_URL+'imageupload',
+      //   filebrowserFlashUploadUrl : BASE_URL+'imageupload'
+	   
+ });
+
+
+
+
+
